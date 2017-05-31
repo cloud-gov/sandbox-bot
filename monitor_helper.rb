2d8cb31cb50c90ddb3949b9c6138c3c361893ef8
Chris Nelson <christopher.nelson@gsa.gov>
2017-05-31 09:54:15 -0700

    fix insane indenting

--- monitor_helper.rb
+++ monitor_helper.rb
@@ -13,54 +13,43 @@ module MonitorHelper
     @@domains = ['.gov', '.mil']
   end
 
-	def is_valid_email(username)
+  def is_valid_email(username)
+    !username.nil? && !username.index("@").nil?
+  end
 
-		!username.nil? && !username.index("@").nil?
-
-	end
-
-	def is_whitelisted_email(email)
-
+  def is_whitelisted_email(email)
     @@domains.any? do |domain|
       email.downcase.end_with?(domain)
     end
+  end
 
-	end
+  # Extracts the domain name (minus the top level domain) from
+  # an email address. e.g. foo@subdomain.domain.org = domain
 
-	# Extracts the domain name (minus the top level domain) from
-	# an email address. e.g. foo@subdomain.domain.org = domain
-
-	def get_email_domain_name(email)
-
-	tlds = ['.gov', '.mil', '.fed.us']
+  def get_email_domain_name(email)
+    tlds = ['.gov', '.mil', '.fed.us']
 
     domain = email.split('@')[1]
 
     # remove any tlds
-	tlds.each{|t|
-		domain.slice! t
-	}
+    tlds.each{|t|
+      domain.slice! t
+    }
 
-	# return what we know about
+    # return what we know about
     domain.split('.')[-1]
+  end
 
-	end
+  def get_sandbox_space_name(email)
+    return email.split('@')[0].downcase
+  end
 
-	def get_sandbox_space_name(email)
+  def get_cloud_environment(uaa_url)
+    environment = 'unknown'
+    if (uaa_url.index("uaa."))
+      environment = uaa_url[uaa_url.index("uaa.")+4..-1]
+    end
 
-   		return email.split('@')[0].downcase
-
-	end
-
-	def get_cloud_environment(uaa_url)
-
-		environment = 'unknown'
-		if (uaa_url.index("uaa."))
-			environment = uaa_url[uaa_url.index("uaa.")+4..-1]
-		end
-
-		environment
-
-	end
-
+    environment
+  end
 end
